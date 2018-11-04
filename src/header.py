1e4864f034bdbb3202570ae3ddb52fb1ce296ece
ntuanhung <ntuanhung@users.noreply.github.com>
2018-11-04 12:55:25 +0900

    Update header.py

--- src/header.py
+++ src/header.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import numpy as np
 from tqdm import tqdm
 
@@ -7,23 +6,10 @@ import math
 import time
 import os
 import six
-import chainer
-import chainer.functions as F
-import chainer.links as L
-import zipfile
-from chainer.links import caffe
-from chainer import link, Chain, optimizers, Variable, initializers, training
 from PIL import Image
 import pickle
 import gzip
-import h5py
-#import leveldb
 from scipy import ndimage, misc
-try:
-    import cupy
-except:
-    xp=np
-
 import logging
 from datetime import datetime
 import argparse
